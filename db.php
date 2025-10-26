db6dd3657aef1485ea7ccb4b0055a99eed87b53f
Ahabd4711 <af8240892@gmail.com>
2025-10-26 14:53:52 +0100

    Initialize database connection in db.php

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root"; // standardbruker i XAMPP
+$pass = "";     // tomt passord som standard
+$db = "ahabd4711"; // navnet på databasen du lagde
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Feil ved tilkobling: " . $conn->connect_error);
+}
+?>
